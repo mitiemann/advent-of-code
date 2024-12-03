edc121cc92c98cb9ca64708249e237d5d50f715c
Michael Tiemann <github@michaeltiemann.net>
2024-12-03 22:42:17 +0100

    WIP working on day 03

---
+++ 2024/day-03.jl
@@ -0,0 +1,28 @@
+### A Pluto.jl notebook ###
+# v0.20.3
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ bc69f208-b1be-11ef-3717-a7dc40f55551
+md"# Advent of Code - Day 03"
+
+# ╔═╡ 10151854-e09e-4de0-a103-18c9041e4371
+md"## Part 1"
+
+# ╔═╡ 8028a763-e87d-4ffc-b8e9-6a750edf9c03
+begin
+	f = open("day-03-input.txt","r")
+	lines = readlines(f)
+	close(f)
+	lines
+end
+
+# ╔═╡ c1129cab-d4f0-41b1-b258-8cce310da436
+lines[1]
+
+# ╔═╡ Cell order:
+# ╟─bc69f208-b1be-11ef-3717-a7dc40f55551
+# ╟─10151854-e09e-4de0-a103-18c9041e4371
+# ╠═8028a763-e87d-4ffc-b8e9-6a750edf9c03
+# ╠═c1129cab-d4f0-41b1-b258-8cce310da436
